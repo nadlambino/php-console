201d4af25b0e211588f31f96ec55072cf9c1ede0
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2023-12-30 12:35:46 +0800

    ref: formatting and docblocks

--- Commands/Command.php
+++ Commands/Command.php
@@ -7,13 +7,37 @@ namespace Inspira\Console\Commands;
 use Inspira\Console\Input;
 use Inspira\Console\Output;
 
+/**
+ * Class Command
+ *
+ * Abstract base class for console commands.
+ *
+ * @package Inspira\Console\Commands
+ */
 abstract class Command implements CommandInterface
 {
+	/**
+	 * @var string The description of the command.
+	 */
 	protected string $description = '';
 
+	/**
+	 * @var array The required parameters for the command.
+	 */
 	protected array $requires = [];
 
+	/**
+	 * @var array The optional parameters for the command.
+	 */
 	protected array $optionals = [];
 
-	public function __construct(protected Input $input, protected Output $output) { }
+	/**
+	 * Command constructor.
+	 *
+	 * @param Input $input The input instance for handling command input.
+	 * @param Output $output The output instance for displaying command output.
+	 */
+	public function __construct(protected Input $input, protected Output $output)
+	{
+	}
 }
