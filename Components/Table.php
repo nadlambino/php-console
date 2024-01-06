9d1fa56b5a8335ed953c72698c2503d76f3ba13c
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-01-06 19:19:36 +0800

    docs: docblocks

--- Components/Table.php
+++ Components/Table.php
@@ -8,12 +8,39 @@ use Inspira\Console\Contracts\ComponentInterface;
 use Inspira\Console\Enums\Colors;
 use Inspira\Console\Output;
 
+/**
+ * Class Table
+ *
+ * A console component implementation for rendering tabular data in a table format.
+ *
+ * @package Inspira\Console\Components
+ */
 final class Table implements ComponentInterface
 {
-	public function __construct(protected Output $output, protected array $data, protected int $padding = 3)
+	/**
+	 * Table constructor.
+	 *
+	 * @param Output $output The output instance for writing to the console.
+	 * @param array $data The tabular data to be displayed in the table.
+	 * @param int $padding The padding to be applied to each column in the table.
+	 * @param int $coloredPadding The padding to be applied in colored headers.
+	 */
+	public function __construct(
+		protected Output $output,
+		protected array  $data,
+		protected int    $padding = 3,
+		protected int    $coloredPadding = 4
+	)
 	{
 	}
 
+	/**
+	 * Render the table by printing headers and body to the console.
+	 *
+	 * If the data is empty, no output will be generated.
+	 *
+	 * @return void
+	 */
 	public function render(): void
 	{
 		if (empty($this->data)) {
@@ -28,6 +55,12 @@ final class Table implements ComponentInterface
 			->printBody($columnWidths);
 	}
 
+	/**
+	 * Get the headers of the table based on the first row of data.
+	 * This assumes that the items from the data are in similar structure.
+	 *
+	 * @return array The headers of the table.
+	 */
 	protected function getHeaders(): array
 	{
 		$columns = array_keys($this->data[0] ?? []);
@@ -38,9 +71,16 @@ final class Table implements ComponentInterface
 		}
 
 		return $headers;
-
 	}
 
+	/**
+	 * Prepend headers to the data array.
+	 * This is to include the headers when counting the longest character in each column.
+	 *
+	 * @param array $headers The headers to be prepended.
+	 *
+	 * @return $this
+	 */
 	protected function appendHeaders(array $headers): self
 	{
 		array_unshift($this->data, $headers);
@@ -48,6 +88,12 @@ final class Table implements ComponentInterface
 		return $this;
 	}
 
+	/**
+	 * Remove the headers from the data array.
+	 * After the characters has been counted, we now need to remove the prepended headers to avoid printing it out.
+	 *
+	 * @return $this
+	 */
 	protected function removeHeaders(): self
 	{
 		array_shift($this->data);
@@ -55,6 +101,12 @@ final class Table implements ComponentInterface
 		return $this;
 	}
 
+	/**
+	 * Get the longest character count for each column in the table data.
+	 * The data should also include the headers to properly align the headers when printing out.
+	 *
+	 * @return array The widths of each column.
+	 */
 	protected function getColumnWidths(): array
 	{
 		$widths = [];
@@ -71,10 +123,19 @@ final class Table implements ComponentInterface
 		return $widths;
 	}
 
+	/**
+	 * Print the headers to the console with proper formatting and coloring.
+	 *
+	 * @param array $columnWidths The widths of each column.
+	 *
+	 * @return $this
+	 */
 	protected function printHeaders(array $columnWidths): self
 	{
 		foreach (array_keys($columnWidths) as $column) {
-			$text = str_pad($this->output->colorize(ucwords($column), Colors::GREEN), $columnWidths[$column] + strlen(Colors::GREEN->value) + 4);
+			$coloredColumn = $this->output->colorize(ucwords($column), Colors::GREEN);
+			$width = $columnWidths[$column] + strlen(Colors::GREEN->value) + 4;
+			$text = str_pad($coloredColumn, $width);
 			$this->output->write($text);
 		}
 		$this->output->eol();
@@ -82,6 +143,13 @@ final class Table implements ComponentInterface
 		return $this;
 	}
 
+	/**
+	 * Print the body of the table to the console.
+	 *
+	 * @param array $columnWidths The widths of each column.
+	 *
+	 * @return $this
+	 */
 	protected function printBody(array $columnWidths): self
 	{
 		foreach ($this->data as $row) {
--- Contracts/ComponentInterface.php
+++ Contracts/ComponentInterface.php
@@ -4,7 +4,22 @@ declare(strict_types=1);
 
 namespace Inspira\Console\Contracts;
 
+/**
+ * Interface ComponentInterface
+ *
+ * This interface defines the contract for console components that can be rendered.
+ *
+ * @package Inspira\Console\Contracts
+ */
 interface ComponentInterface
 {
+	/**
+	 * Render the console component.
+	 *
+	 * Implementing classes should provide the logic for rendering the component.
+	 * This method is responsible for outputting the content of the component to the console.
+	 *
+	 * @return void
+	 */
 	public function render(): void;
 }
